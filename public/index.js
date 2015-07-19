622e0e3f532982c8e292b8d0b22a74815590ea8c
Valentyn Shybanov <olostan@gmail.com>
2015-07-19 06:15:43 +0300

    Chat

--- public/index.js
+++ public/index.js
@@ -1,3 +1,32 @@
+
+function get(url) {
+  return fetch(url,{credentials: 'same-origin'} );
+}
+function postJSON(url,data) {
+  return fetch(url,{
+    credentials: 'same-origin',
+    method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  });
+}
+function post(url,data) {
+  return fetch(url,{
+    credentials: 'same-origin',
+    method: 'post',
+      headers: {
+        'Accept': 'text/plain',
+        'Content-Type': 'text/plain'
+      },
+      body: data
+  });
+}
+
+
+var fetchPostOptions = {credentials: 'same-origin'};
 var content = document.getElementById('content');
 function addLine(str) {
    var newLine = document.createElement('div');
@@ -24,13 +53,48 @@ input.addEventListener('keypress', function(e){
 input.addEventListener('keyup',  function(e) {
    if(e.keyCode!=13) return;
    var str = input.innerText;
-   addLine("You entered:"+str);
+   //addLine("You entered:"+str);
+   post("/api/message",str);
    input.innerHTML = '';
    focusInput();
 
 });
 setTimeout(focusInput,100);
-
-fetch('/api/',{credentials: 'same-origin'}).then(function(r) {
+get('/api/user').then(function(r) {
   return r.text();
 }).then(function(t) { addLine("Message from server:"+t);});
+
+
+
+var channel;
+function join() {
+    get('/api/join').then(function(r) { return r.text(); }).then(function(token) {
+        console.log("joining", token);
+        channel = new goog.appengine.Channel(token);
+        socket = channel.open();
+        socket.onopen = onOpened;
+        socket.onmessage = onMessage;
+        socket.onerror = onError;
+        socket.onclose = onClose;
+    });
+}
+function onOpened() {
+    addLine("Channel opened");
+}
+function sanitize(txt) {
+   var d = document.createElement('div');
+   d.innerHTML = txt;
+   return d.innerText;
+}
+function onMessage(m) {
+    var message = JSON.parse(m.data);
+    console.log(m);
+    addLine("<span class='name'>"+message.Sender+"</span>: <span class='message'>"+message.Text+"</span>");
+}
+function onError() {
+    addLine("Error in channel");
+}
+function onClose() {
+    addLine("Channel closed");
+}
+join();
--- server.go
+++ server.go
@@ -5,24 +5,57 @@ import (
 	"appengine/user"
 	"fmt"
 	"net/http"
+	"appengine/channel"
+	"html"
 )
 
 func init() {
-	http.HandleFunc("/api/", handler)
+	http.HandleFunc("/api/user", userHandler)
+	http.HandleFunc("/api/join", joinHandler)
+	http.HandleFunc("/api/message", messageHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func userHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
-	if u == nil {
-		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
+	fmt.Fprintf(w, "Hello, %v!", u)
+}
+
+func joinHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	tok, err := channel.Create(c, "general")
+	if err != nil {
+		http.Error(w, "Couldn't create Channel", http.StatusInternalServerError)
+		c.Errorf("channel.Create: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "Hello, %v!", u)
+	fmt.Fprintf(w, tok)
+}
+func readBody(r *http.Request) (string,error) {
+	p := make([]byte, r.ContentLength-1)
+	_, err := r.Body.Read(p)
+	if err != nil {return "", err;}
+	s := string(p);
+	return s,nil;
+}
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	message, err := readBody(r);
+	message = html.EscapeString(message);
+	if err != nil {
+		http.Error(w, "Couldn't read message", http.StatusInternalServerError)
+		c.Errorf("Reading body: %v", err)
+		return
+	}
+	err = channel.SendJSON(c, "general",Message{u.Email,message})
+	if err != nil {
+		c.Errorf("sending Game: %v", err)
+	}
+	fmt.Fprintf(w, "Ok")
+}
+
+type Message struct {
+	Sender string
+	Text string
 }
